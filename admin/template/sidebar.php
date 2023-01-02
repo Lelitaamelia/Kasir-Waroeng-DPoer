1603334aae414c77720f7287dc5cf94a7334abac
yuyunine26 <117963966+yuyunine26@users.noreply.github.com>
2023-01-02 17:41:59 +0700

    template

--- admin/template/sidebar.php
+++ admin/template/sidebar.php
@@ -20,7 +20,6 @@
     <!-- Nav Item - Dashboard -->
     <li class="nav-item active">
         <a class="nav-link" href="index.php">
-            
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
     </li>
@@ -31,7 +30,22 @@
     <!-- <div class="sidebar-heading">
            Master
        </div> -->
-    
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item active">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
+            aria-controls="collapseTwo">
+            <i class="fas fa-fw fa-database"></i>
+            <span>Master Data</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <!-- <h6 class="collapse-header">Custom Components:</h6> -->
+                <a class="collapse-item" href="index.php?page=barang">Data Barang</a>
+                <a class="collapse-item" href="index.php?page=kategori">Kategori</a>
+                <!-- <a class="collapse-item" href="index.php?page=user">User</a> -->
+            </div>
+        </div>
+    </li>
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item active">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapse3" aria-expanded="true"
@@ -43,7 +57,7 @@
             <div class="bg-white py-2 collapse-inner rounded">
                 <!-- <h6 class="collapse-header">Custom Components:</h6> -->
                 <a class="collapse-item" href="index.php?page=jual">Transaksi Jual</a>
-              
+                <a class="collapse-item" href="index.php?page=laporan">Laporan Penjualan</a>
             </div>
         </div>
     </li>
